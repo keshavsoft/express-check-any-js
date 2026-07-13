2b45b0846db90d69abbd62df402099b3d802c8ea
keshavsoft <nknnkumar@yahoo.com>
2026-07-13 12:38:02 +0530

    v8

---
+++ test/v8/checkImport/check/index.js
@@ -0,0 +1,13 @@
+import { getAllImports } from './getAllImports.js';
+import { getImportCount } from './getImportCount.js';
+import { isImportPresent } from './isImportPresent.js';
+import { getImportStartLine } from './getImportStartLine.js';
+import { getImportEndLine } from './getImportEndLine.js';
+
+export {
+    getAllImports,
+    getImportCount,
+    isImportPresent,
+    getImportStartLine,
+    getImportEndLine
+};
